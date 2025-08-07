2f544cc22fa2c410d06cfa0beddcbf32fcb17e95
Lee-SangUk-theAwesome <bleach1491@naver.com>
2025-08-07 17:36:24 +0900

    Revert "1"
    This reverts commit ec3c0c2372d514a9f90e390a3cdf394823c61d7b.

--- docs/humortube/vite.config.js
+++ docs/humortube/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/html5/humortube/',
   plugins: [react()],
 })
+
+base: '/html5/humortube/', 
